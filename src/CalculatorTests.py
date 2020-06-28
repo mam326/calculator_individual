1b5dc23c53b9cc0ab11b2a1f3cb836f61af7e1e9
mam32 <mam326@njit.edu>
2020-06-28 11:03:07 -0400

    Feature: Set up Multiplication - testok

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -3,6 +3,9 @@ from Calculator import Calculator
 
 class MyTestCase(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
     def test_instantiate_calculator(self):
         calculator = Calculator()
         self.assertIsInstance(calculator, Calculator)
@@ -20,6 +23,13 @@ class MyTestCase(unittest.TestCase):
         calculator = Calculator()
         self.assertEqual(calculator.subtract(2, 2), 0)
         self.assertEqual(calculator.result, 0)
+
+
+    def test_multiply_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.multiply(2, 2), 4)
+        self.assertEqual(calculator.result, 4)
+
 
 if __name__ == '__main__':
     unittest.main()
