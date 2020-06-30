20122f9c50a94b32b850a0802ae36dea96dd05e4
mam32 <mam326@njit.edu>
2020-06-30 11:26:47 -0400

    Fix:  Updated CSV Reader to use  external absolutepath

--- src/CsvReader.py
+++ src/CsvReader.py
@@ -1,16 +1,16 @@
 import csv
 from pprint import pprint
-
+from absolutepath import absolutepath
 
 def ClassFactory(class_name, dictionary):
     return type(class_name, (object,), dictionary)
-
 
 class CsvReader:
     data = []
 
     def __init__(self, filepath):
-        with open(filepath) as text_data:
+        self.data = []
+        with open(absolutepath(filepath)) as text_data:
             csv_data = csv.DictReader(text_data, delimiter=',')
             for row in csv_data:
                 self.data.append(row)
